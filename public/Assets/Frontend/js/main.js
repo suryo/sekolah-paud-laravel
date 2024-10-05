3fef2da7c6e2b1d776bcc0e655a855b8875e6096
suryo <suryoatm@gmail.com>
2024-10-05 17:42:22 +0700

    modified url mobile

--- public/Assets/Frontend/js/main.js
+++ public/Assets/Frontend/js/main.js
@@ -138,7 +138,7 @@
     /*-------------------------------------
      jQuery MeanMenu activation code
      --------------------------------------*/
-    $('nav#dropdown').meanmenu({ siteLogo: "<a href='index.html' class='logo-mobile-menu'><img src='img/mobile-logo.png' /></a>" });
+    $('nav#dropdown').meanmenu({ siteLogo: "<a href='/' class='logo-mobile-menu'><img src='img/mobile-logo.png' /></a>" });
 
     /*-------------------------------------
      Wow js Active
